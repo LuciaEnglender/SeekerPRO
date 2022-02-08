d5b920a1b4862031f2cd5443f5483ea9a92e835a
Franco Montini <devmontini@gmail.com>
2022-02-08 13:44:40 -0300

    Update ButtonLogIn.jsx

--- client/src/private/ButtonLogIn.jsx
+++ client/src/private/ButtonLogIn.jsx
@@ -5,7 +5,7 @@ const ButtonLogIn = ({ estilo }) => {
   const { loginWithRedirect } = useAuth0();
   return (
     <button
-      className={estilo}
+      className="p-4 py-2 inline-block bg-gradient-to-r from-verdeClaro to-verdeMedio text-white font-bold rounded-3xl filter hover:drop-shadow"
       onClick={() => loginWithRedirect()}
       id="qsLoginBtn"
     >
