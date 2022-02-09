0cfb19d19fd5ca595b55033f7cd775bc410ecac5
Franco Montini <devmontini@gmail.com>
2022-02-09 15:51:19 -0300

    asdasd
    asdasd

--- client/src/component/pages/Landing.jsx
+++ client/src/component/pages/Landing.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ButtonLogIn, ButtonLogOutLanding } from "../../private/ButtonLogIn";
 import { useAuth0 } from "@auth0/auth0-react";
-import { getUsers, postEmail } from "../../redux/actions/indexL";
+import { getUsers } from "../../redux/actions/indexL";
 import { useDispatch, useSelector } from "react-redux";
+import SectionNuevo from "./SectionNuevo";
+import SectionViejo from "./SectionViejo";
 import ButtonSignIn from "../../private/ButtonSignIn";
-import { ButtonsHomeE, ButtonsHomeP } from "../../private/ButtonsHome";
 
 const Landing = () => {
   const dispatch = useDispatch();
@@ -14,40 +15,12 @@ const Landing = () => {
   );
   const { user, isAuthenticated } = useAuth0();
 
+  // const email = profileState?.mail;
+
   useEffect(() => {
     dispatch(getUsers(profileState.mail));
-  });
-
-  const [input, setInput] = useState({
-    name: "",
-    email: "",
-    photo: "",
-    profile: "",
-  });
-
-  function handleRecruiter(e) {
-    e.preventDefault();
-    dispatch(postEmail(input));
-    console.log(input);
-    setInput({
-      name: user.name,
-      email: user.email,
-      photo: user.picture,
-      profile: "BUSINESS",
-    });
-  }
-
-  function handleDeveloper(e) {
-    e.preventDefault();
-    dispatch(postEmail(input));
-    console.log(input);
-    setInput({
-      name: user.name,
-      email: user.email,
-      photo: user.picture,
-      profile: "DEVELOPER",
-    });
-  }
+    console.log(profileState.mail);
+  }, [dispatch]);
 
   return (
     <body className="p-9 bg-gray-300">
@@ -78,68 +51,9 @@ const Landing = () => {
       <selection className="px-16 mt-32 mb-32">
         {isAuthenticated ? (
           profileState.profile === null ? (
-            //NUEVO USUARIO
-            <div className="grid grid-cols-2">
-              <div className="p-2">
-                <h2 className="text-5xl  font-bold pb-4">Developer?</h2>
-                <p className="pb-4">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tempora, sapiente vero temporibus ullam voluptatibus modi
-                  maxime quis minima dicta iure hic, molestiae libero veritatis
-                  quos.
-                </p>
-                <Link to="/homep/create">
-                  {" "}
-                  <button
-                    className="p-4 ml-2 py-2 inline-block bg-gradient-to-r to-verdeClaro from-verdeMedio text-white font-bold rounded-3xl filter hover:drop-shadow"
-                    onClick={(e) => handleDeveloper(e)}
-                  >
-                    Empresa...
-                  </button>
-                </Link>
-              </div>
-              <div className="p-2">
-                <h2 className="text-5xl font-bold pb-4">Recruiter?</h2>
-                <p className="pb-4">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tempora, sapiente vero temporibus ullam voluptatibus modi
-                  maxime quis minima dicta iure hic, molestiae libero veritatis
-                  quos.
-                </p>
-                <Link to="/homee/create">
-                  <button
-                    className="p-4 ml-2 py-2 inline-block bg-gradient-to-r to-verdeClaro from-verdeMedio text-white font-bold rounded-3xl filter hover:drop-shadow"
-                    onClick={(e) => handleRecruiter(e)}
-                  >
-                    Recruitere...
-                  </button>
-                </Link>
-              </div>
-            </div>
+            <SectionNuevo></SectionNuevo>
           ) : (
-            //VIEJO USUARIO
-            <div className="grid grid-cols-2">
-              <div>
-                <h2 className="text-5xl font-bold pb-4">Welcome!</h2>
-                <p className="pb-4">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tempora, sapiente vero temporibus ullam voluptatibus modi
-                  maxime quis minima dicta iure hic, molestiae libero veritatis
-                  quos.
-                </p>
-
-                <h3 className="text-xl pl-3 font-bold pb-4">
-                  {profileState.profile === "BUSINESS" ? (
-                    <ButtonsHomeE />
-                  ) : (
-                    <ButtonsHomeP />
-                  )}
-                </h3>
-              </div>
-              <div>
-                <img className="max-w-sm" src="/Landing.png" alt="asd" />
-              </div>
-            </div>
+            <SectionViejo></SectionViejo>
           )
         ) : (
           <div className="grid grid-cols-2">
@@ -151,13 +65,9 @@ const Landing = () => {
                 quis minima dicta iure hic, molestiae libero veritatis quos.
               </p>
 
-              <div className="grid grid-cols-2">
-                <div>
-                  <h3 className="text-xl pl-3 font-bold pb-4">
-                    Join Us! <ButtonSignIn></ButtonSignIn>
-                  </h3>
-                </div>
-              </div>
+              <h3 className="text-xl pl-3 font-bold pb-4">
+                Join Us! <ButtonSignIn></ButtonSignIn>
+              </h3>
             </div>
             <div>
               <img className="max-w-sm" src="/Landing.png" alt="asd" />
--- client/src/private/Register.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-//mail pass perfil (action GET)
-
-const Register = () => {
-  return <div>{}</div>;
-};
-
-export default Register;
