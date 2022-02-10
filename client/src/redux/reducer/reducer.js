4d4d363fff0761de0022f59fb67373fbb317616b
Lucia Englender <luenglender@gmail.com>
2022-02-10 14:10:43 -0300

    delet client
    deleting client

--- client/src/redux/reducer/reducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-import rootReducer from '../reducer'
-import rootReducerPostulante from '../reducer/indexP'
-
-
-export default combineReducers({
-    rootReducer, 
-    rootReducerPostulante
-})
