e20f05ba5eb7d63a59ab85fcefc2e4001959819b
valentinaq13 <emeritavalentina@gmail.com>
2022-02-10 13:27:49 -0300

    continue
    continue

--- client/src/component/empresas/DetailVacy.jsx
+++ client/src/component/empresas/DetailVacy.jsx
@@ -17,39 +17,31 @@ function DetailVacy() {
     dispatch(getVacancyDetail(id))
   }, [dispatch, id])
   console.log(detalle)
- 
-  var count = 0
-  
-  return (
-    <div>
-      <div>
-      <NavHomeE titulo={"Notificaciones"} />
-      </div>
-      <div>
-        <Pipeline />
-      </div>
-      <div key={detalle[0].id}>
-        <h2>Buscamos: {detalle[0].name}</h2>
-        <h3>Descripcion de la vacante: {detalle[0].description}</h3>
-        <h4>Seniority: {detalle[0].seniorities.map((ele) => (<p>{ele.name}</p>))}</h4>
-        <p>Tecnologías Requeridas: {detalle[0].technologies.map((ele) => (<p key={count++}>{ele.name}</p>))}</p>
-        <p>Idioma: {detalle[0].languages.map((ele) => (<p>{ele.name}</p>))}</p>
 
 
-      </div>
-      <Link to={`/vacancy/edit/${id}`}>
-      {/* <EditVcancy id={id} /> */}
-        <button className={styles.button}>Edit Vacancy</button>
-        </Link>
-
-      <button className={styles.button}>Delete Vacancy</button>
-      <Link to="/homee">
-        <button>
-          <BsFillArrowLeftSquareFill />
-        </button>
-      </Link>
+  return <div>
+    <NavHomeE titulo={"Notificaciones"} />
+    <Pipeline />
+    <div key={detalle[0]?.id}>
+      <h2>Buscamos: {detalle[0]?.name}</h2>
+      <h3>Descripcion de la vacante: {detalle[0]?.description}</h3>
+      <h4>Seniority: {detalle[0].seniorities.length ? detalle[0].seniorities.map((ele) => ele.name) : <p> No especificado</p>}</h4>
+      <p>Tecnologías Requeridas: {detalle[0].technologies.length ? detalle[0].technologies.map((ele) => ele.name) : <p> No especificado</p>}</p>
+      <p>Idioma: {detalle[0].languages.length ? detalle[0]?.languages.map((ele) => ele.name) : <p> No especificado</p>}</p>
     </div>
-  );
+    <Link to={`/vacancy/edit/${id}`}>
+      {/* <EditVcancy id={id} /> */}
+      <button className={styles.button}>Edit Vacancy</button>
+    </Link>
+
+    <button className={styles.button}>Delete Vacancy</button>
+    <Link to="/homee">
+      <button>
+        <BsFillArrowLeftSquareFill />
+      </button>
+    </Link>
+  </div>
+
 }
 
 export default DetailVacy;
