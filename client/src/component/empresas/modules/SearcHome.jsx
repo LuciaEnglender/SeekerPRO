7f6ef210fce9d9e45a4d06f1702ab28ff1bdff57
valentinaq13 <emeritavalentina@gmail.com>
2022-02-14 18:19:25 -0300

    update que no estaba mergeado

--- client/src/component/empresas/modules/SearcHome.jsx
+++ client/src/component/empresas/modules/SearcHome.jsx
@@ -110,12 +110,12 @@ function SearcHome() {
             <form className=" flex flex-row" onSubmit={(e) => handleSubmit(e)}>
                 <div className="bg-grey 300 p-2 px-10">
                     <div className="bg-verdeMedio rounded-2xl p-6 w-full h-full">
-                        <h1 className=" font-bold  text-center mb-3">Filtrar</h1>
+                        <h1 className=" font-bold  text-center mb-3"></h1>
                         <h1 className=" font-bold  text-center mb-3"> </h1>
                         <hr />
                         <div>
                             <div className="w-full flex flex-col m-0 justify-center pt-5">
-                                <label> Buscar:</label>
+                                <label> search:</label>
                                 <input
                                     className="w-full xl:w-60 m-0 border-verdeMuyClaro rounded-2xl bg-verdeClaro "
                                     type="text"
