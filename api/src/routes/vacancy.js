4c41097dade0cfec9be8f942632f94b717d9e466
Franco Montini <devmontini@gmail.com>
2022-02-14 22:52:20 -0300

    CHICHEUEAAAA
    chau a chicheua

--- api/src/routes/vacancy.js
+++ api/src/routes/vacancy.js
@@ -67,7 +67,6 @@ routerVacancy.get("/", async (req, res) => {
   const { id, business } = req.query;
 
   try {
-
     const ALLVACS = await Vacancy.findAll({
       include: [
         {
@@ -104,10 +103,10 @@ routerVacancy.get("/", async (req, res) => {
           through: {
             attributes: [],
           },
-        }
+        },
       ],
     });
-    
+
     //si tiene id (o sea que se requiere el detalle) entra acá
     if (id) {
       const vacanciesInDB = await Vacancy.findAll({
@@ -150,8 +149,8 @@ routerVacancy.get("/", async (req, res) => {
             through: {
               attributes: [],
             },
-          }
-        ]
+          },
+        ],
       });
       //si no está es porque no existe
       vacanciesInDB
@@ -161,9 +160,9 @@ routerVacancy.get("/", async (req, res) => {
       const finderBusiness = await Business.findOne({
         where: {
           loginEmail: business,
-        }
+        },
       });
- console.log(finderBusiness)
+      console.log(finderBusiness);
       //y sino, devuelve todos las vacantes
       const vacanciesInDB = await Vacancy.findAll({
         where: {
@@ -199,17 +198,17 @@ routerVacancy.get("/", async (req, res) => {
             },
           },
           {
-            model: Business
-          }
+            model: Business,
+          },
         ],
       });
       vacanciesInDB
         ? res.status(200).json(vacanciesInDB)
         : res.status(400).send("there arent any vacancies yet");
     } else {
-     
-      ALLVACS? res.status(200).json(ALLVACS)
-      : res.status(400).send('not vacancies yet')
+      ALLVACS
+        ? res.status(200).json(ALLVACS)
+        : res.status(400).send("not vacancies yet");
     }
   } catch (e) {
     console.log(e);
@@ -551,7 +550,7 @@ routerVacancy.get("/vac/:id", async (req, res) => {
         attributes: ["name"],
       })
       .then((postulant) => {
-        console.log(postulant);
+        // console.log(postulant);
         res.json(postulant.length);
       });
   });
