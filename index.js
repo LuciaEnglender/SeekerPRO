37f0fe18d38997849d310c66a9251622d4e10ce3
Franco Montini <devmontini@gmail.com>
2022-02-22 12:40:48 -0300

    sss
    sss

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log(`%s listening at ${3001}`); // eslint-disable-line no-console
   });
