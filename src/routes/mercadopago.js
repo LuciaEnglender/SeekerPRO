7a6458a4b66e8cd8568b1a78e281154e2661accc
BenjaminBaigorria <85437392+BenjaminBaigorria@users.noreply.github.com>
2022-02-24 11:13:05 -0300

    mercadopago litle changes

--- src/routes/mercadopago.js
+++ src/routes/mercadopago.js
@@ -54,9 +54,9 @@ server.post("/", async (req, res, next) => {
       ],
     },
     back_urls: {
-      success: '/mercadopago/pagos',
-      failure: '/mercadopago/pagos',
-      pending: '/mercadopago/pagos',
+      success: "http://localhost:3001/mercadopago/pagos",
+      failure: "http://localhost:3001/mercadopago/pagos",
+      pending: "http://localhost:3001/mercadopago/pagos",
     },
   };
 
